187ea860bf7961ec6c92162d07253976e4bb9e8e
Keita <keyz@google.com>
2025-07-24 14:27:08 +0800

    Add Google Analytics

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+// --- Element Selectors ---
 const agreeBtn = document.getElementById('agreeBtn');
 const agreeMessage = document.getElementById('agreeMessage');
+const isoLink = document.getElementById('isoLink');
+const twitterLink = document.getElementById('twitterLink');
 
+// --- Event Listeners ---
+
+// Track "I'm Convinced" button clicks
 agreeBtn.addEventListener('click', () => {
+    // Fire Google Analytics event
+    gtag('event', 'convince_click', {
+      'event_category': 'engagement',
+      'event_label': 'Convinced Button'
+    });
+
+    // Animate button and show message
     agreeBtn.classList.add('animate-pulse');
     agreeMessage.textContent = 'Awesome! You are now a formatting champion.';
     agreeMessage.classList.remove('opacity-0');
@@ -9,4 +22,22 @@ agreeBtn.addEventListener('click', () => {
     setTimeout(() => {
         agreeBtn.classList.remove('animate-pulse');
     }, 1500);
+});
+
+// Track clicks on the "Learn more" ISO link
+isoLink.addEventListener('click', () => {
+    // Fire Google Analytics event
+    gtag('event', 'select_content', {
+      'content_type': 'link',
+      'item_id': 'iso_learn_more'
+    });
+});
+
+// Track clicks on the author's Twitter link
+twitterLink.addEventListener('click', () => {
+    // Fire Google Analytics event
+    gtag('event', 'select_content', {
+        'content_type': 'social_link',
+        'item_id': 'twitter_author_link'
+    });
 });
